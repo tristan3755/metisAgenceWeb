8fa6d53e47636aa7784988d65c0c31c026c90f0f
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-07-02 16:14:38 +0200

    agenceProjet

---
+++ js.js
@@ -0,0 +1,31 @@
+let slider=document.querySelector('.slider')
+let taille=slider.clientWidth
+console.log(taille)
+let droite=document.querySelector('.droite')
+let gauche=document.querySelector('.gauche')
+let compteur=0
+if(compteur==0){
+  gauche.style.opacity="0"
+  }
+droite.addEventListener('click',()=>{
+  if(compteur<2){
+  compteur++
+  console.log(compteur)
+  slider.scrollTo(taille*compteur,0)
+  gauche.style.opacity='1'
+}
+if(compteur==2){
+droite.style.opacity="0"
+}
+})
+gauche.addEventListener('click',()=>{
+  if(compteur>0){
+  compteur--
+  console.log(compteur)
+  slider.scrollTo(taille*compteur,0)
+  droite.style.opacity='1'
+  } 
+  if(compteur==0){
+    gauche.style.opacity="0"
+    }
+})
